50d8b9128c4bfe19a96c88d66073c9036c3bf760
andongob <ndongo@gmail.com>
2025-01-19 11:52:29 +0100

    Subida inicial del proyecto Flask

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    video_url = request.args.get('url', None)
+    return render_template('index.html', video_url=video_url)
+
+if __name__ == '__main__':
+    # Usa el puerto especificado por Hugging Face o el puerto 5000 por defecto
+    port = int(os.environ.get('PORT', 7860))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+'''
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -9,3 +26,5 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''
